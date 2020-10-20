53bdc6428bde3cca99d93622bba3cfc6f49c90b6
pbshgthm <poobeshgowtham@live.com>
2020-10-20 15:32:52 +0530

    image-lqip-check

--- components/ProjectComp.js
+++ components/ProjectComp.js
@@ -2,22 +2,30 @@ import styles from '../styles/Project.module.css'
 import {useState,useEffect} from 'react'
 const Img=(props)=>{
 	const [imageLoaded, setImageLoaded] = useState(false);
-	
+	const [loadImg,setLoadImg] = useState(false);
+
+
 	const srcStr=props.src.split('#')
 	return(
 		<div className={styles.imgContainer}>
 			<img 
 				className={styles.lqipNormal}
 				src={require(`../content/works/${srcStr[0]}?lqip`)} 
-				style={{opacity:(imageLoaded?"0.3":"0.7")}}
+				style={{opacity:(imageLoaded?"0.3":"0.7"),zIndex:'4'}}
 				onLoad={() => console.log('lo')}
 			/>
-			<img
+			<img 
+				className={styles.lqipNormal}
+				src={require(`../content/works/${srcStr[0]}?lqip`)} 
+				style={{opacity:'0',position:'relative'}}
+				onLoad={() => console.log('lo')}
+			/>
+			{<img
         className={styles.imgNormal}
         src={require(`../content/works/${srcStr[0]}`)}
         alt={props.alt}
 				onLoad={() => {console.log('loaded');setImageLoaded(true)}}
-      />
+      />}
 		</div>
 	)
 }
