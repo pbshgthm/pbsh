512bb63136171ede29350882ae6c78068c5c5ae1
pbshgthm <poobeshgowtham@live.com>
2020-12-20 06:05:40 +0530

    pure-css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
-  purge: [],
+  purge: {
+    content: ['./components/**/*.js', './pages/**/*.js'],
+  },
   darkMode: false,
   theme: {
 		extend: {
@@ -38,5 +40,20 @@ module.exports = {
 
       addUtilities(newUtilities)
     }),
+		[
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+            // the paths to all template files
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}",
+            ],
+            // function used to extract class names from the templates
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+    ],
   ],
 }
